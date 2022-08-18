3df535f19fcf7452bdaadc7276382df15cd03ae2
allaiquec <88158031+allaiquec@users.noreply.github.com>
2022-08-18 17:16:34 -0500

    Update Persona.java

--- src/main/java/com/example/project/Persona.java
+++ src/main/java/com/example/project/Persona.java
@@ -7,4 +7,22 @@ public class Persona {
         this.DNI = DNI;
         this.nombre = nombre;
     }
+    
+    //Getters y Setters
+    
+    public void setDNI(String DNI) {
+        this.DNI = DNI;
+    }
+
+    public String getDNI() {
+        return this.DNI;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return this.nombre;
+    }
 }
